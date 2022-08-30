95b91e22417f2a019b8feac23f6e0cd4af2e00fe
ViggxDev <81710770+ViggxDev@users.noreply.github.com>
2022-08-30 22:04:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ client = commands.Bot(command_prefix = '@')
 
 messageSent = False
 
-immune = ["Viggx#6671", "Peja#0243", "jöken#9269", "Deriyxl#1726", "Ghost Ping Remover#7921"] 
+immune = ["UsernameHere"] 
 
 @client.event
 async def on_ready():
@@ -33,22 +33,21 @@ async def on_member_remove(member):
 async def on_message(message):
     messageSent = False
     messageAuthor = str(message.author)
-    channel = client.get_channel(826858593294221312)
-    if messageAuthor != "Ghost Ping Remover#7921":
+    channel = client.get_channel(XXX)
+    if messageAuthor != "Botname":
         for x in immune:
             if str(message.author) == x:
                 print(f'{message.author} is immune!')
                 await message.author.send(f'{messageAuthor}: {message.content}  -  You are immune to warnings tho!')
                 messageSent = True
-                if messageAuthor != "Viggx#6671" and messageAuthor != "Ghost Ping Remover#7921":
+                if messageAuthor != "Username" and messageAuthor != "Botname":
                     await message.channel.purge(limit=1)
             else:
                 if messageSent == False:
                     if message.channel != channel:
                         if "@everyone" in message.content:
                             print(f'{message.author}:{message.content}')
-                            #await message.channel.send(f'@Viggx#6671  @{messageAuthor} metioned @.everyone')
-                            channel = client.get_channel(826858593294221312)
+                            channel = client.get_channel(XXX)
                             await channel.send(f'{messageAuthor}: {message.content}  -   (SENT IN: #{message.channel})  -   {datetime.now().strftime("%d/%m/%Y - %H:%M:%S")}')
                             await message.author.send(f'Explain yourself: {messageAuthor}: {message.content}  -  {datetime.now().strftime("%d/%m/%Y - %H:%M:%S")}')
                             await message.channel.purge(limit=1)
